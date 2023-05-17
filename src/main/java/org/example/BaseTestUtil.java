08057169d870141b4e9d0836b6f9b9c51325e0c0
KavyaShree <kavyashree2614@gmail.com>
2023-05-17 16:31:08 +0530

    Test
    Test

---
+++ src/main/java/org/example/BaseTestUtil.java
@@ -0,0 +1,22 @@
+package org.example;
+
+import java.util.ArrayList;
+
+public class BaseTestUtil {
+    static String[] lst = {"S1", "S2", "S3", "S4"};
+    public static ArrayList<String> playThreeSongs(){
+        ArrayList<String> emp=new ArrayList<>();
+        for(int i=0; i<3; i++){
+            emp.add(lst[i]);
+        }
+        return emp;
+    }
+
+    public static ArrayList<String> songList(String song, ArrayList<String> snglst1){
+        ArrayList<String> snglist1=snglst1;
+        snglist1.set(0, snglist1.get(1));
+        snglist1.set(1, snglist1.get(2));
+        snglist1.set(2, song);
+        return snglist1;
+    }
+}
---
+++ src/test/java/org/example/LoopPlaylist.java
@@ -0,0 +1,36 @@
+package org.example;
+
+import com.sun.javafx.util.Utils;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import java.util.ArrayList;
+
+public class LoopPlaylist extends BaseTestUtil {
+
+
+   @Test
+    public void verifyUserHasPlayed3Songs() {
+       System.out.println("user has played 3 songs "+ playThreeSongs());
+       Assert.assertEquals(playThreeSongs().size(),3);
+    }
+
+    @Test
+    public void verifyPlaylistSequenceWhen_S4_S2_S1_PlayedOneAfterOther() {
+
+        //When S4 Played after 3 songs
+        ArrayList<String> s4 = songList("S4",playThreeSongs() );
+        String[] expectedLists4 = {"S2", "S3", "S4"};
+        Assert.assertEquals(s4.toArray(),expectedLists4);
+
+        //When S2 played after S4
+        String[] expectedLists2 = {"S3", "S4", "S2"};
+        ArrayList<String> s2 = songList("S2",s4 );
+        Assert.assertEquals(s2.toArray(),expectedLists2);
+
+        //When S1 played after S2
+        String[] expectedLists1 = {"S4", "S2", "S1"};
+        ArrayList<String> s1 = songList("S1",s2 );
+        Assert.assertEquals(s1.toArray(),expectedLists1);
+    }
+}
